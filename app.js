3ecf8cc1afb25509460a3f280e174062ce28ab08
Alhuar <alfonsohuescar@gmail.com>
2017-03-24 21:18:05 +0100

    remove unecessary code

--- app.js
+++ app.js
@@ -13,12 +13,6 @@ if (process.env.NODE_ENV !== 'test') {
   mongoose.connect(process.env.MONGODB_URI);
 }
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
